b566a804f08ca13753f98b2951ed3531c0be122b
udany <long.eagle.goodies@gmail.com>
2019-08-16 01:07:54 -0300

    Basic setup

---
+++ client/src/layouts/DefaultLayout.vue
@@ -0,0 +1,28 @@
+<template>
+	<b-container class="layout-default" fluid>
+		<!-- Header -->
+		<b-container fluid>
+			<b-row align-h="between">
+				<b-col cols="auto" class="p-0">
+					HEADER
+				</b-col>
+			</b-row>
+		</b-container>
+
+		<!-- Content -->
+		<b-container fluid >
+			<b-row class="rb-content-page">
+				<slot></slot>
+			</b-row>
+		</b-container>
+	</b-container>
+</template>
+
+<script>
+	export default {
+		name: 'DefaultLayout'
+	}
+</script>
+
+<style lang="scss" scoped>
+</style>
